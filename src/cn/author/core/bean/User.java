db48931277b214d74ef5b8357a07c9ab17a62946
author <liang6.-0@163.com>
2017-08-26 19:24:10 +0800

    mybatis sqlmap generator init project

---
+++ src/cn/author/core/bean/User.java
@@ -0,0 +1,45 @@
+package cn.author.core.bean;
+
+import java.util.Date;
+
+public class User {
+    private Integer id;
+
+    private String pwd;
+
+    private String name;
+
+    private Date date;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd == null ? null : pwd.trim();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
